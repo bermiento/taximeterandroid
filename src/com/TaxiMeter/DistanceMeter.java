ebc107365ff809287b1c5a175a4cb49d7fea0242
ybudweiser@gmail.com <ybudweiser@gmail.com@fb55d423-cd0a-6e09-8d2d-4d462db0f645>
2010-10-06 13:54:31 +0000

    Updated Distance Meter--Still Requires More Work

--- src/com/TaxiMeter/DistanceMeter.java
+++ src/com/TaxiMeter/DistanceMeter.java
@@ -2,8 +2,45 @@ package com.TaxiMeter;
 
 public class DistanceMeter {
 	private DistanceUnit myRates;
+	private long myPrice;
+	private long feetTraveled;
+	private long initialTime;
 	public DistanceMeter(DistanceUnit myRates){
 		this.myRates=myRates;
+		myPrice=0;
+		initialTime=System.nanoTime();
 	}
+	public long getWholeTimeUnits(){
+		return feetTraveled/myRates.getDistance();
+	}
+	public long getHalfTimeUnits(){
+		return feetTraveled%myRates.getDistance();
+	}
+	public void incrementMeter(//currentSpeed
+			){
+		long newTime=System.nanoTime();
+		//subtract NewTimefrom initialTime and multiply by currentspeed to get the distance
+		//save the NewTime as initialTime
+		long TimeDifference=initialTime-System.nanoTime();
+		//todo stub
+		//TimeDifference*currentSpeed(convert to feet/hr)
+		initialTime=newTime;
+		
+	}
+	/**
+	 * 
+	 * @return the current Price based on the whole billing units + the half billing units multiplied by the price per unit
+	 */
+	public long getPrice(){
+		myPrice=myRates.getPricePerUnit()*(  this.getWholeTimeUnits()  +this.getHalfTimeUnits() );
+		return myPrice;
+	}
+	public String toString(){
+		return "TimeMeter Price: "+myPrice+" cents.\n"+
+		"Number of whole billing units passed: "+this.getWholeTimeUnits()+
+		"Number of half Billing units passed: "+this.getHalfTimeUnits()+
+		myRates;
+	}
+	
 	
 }
